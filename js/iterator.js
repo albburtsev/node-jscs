55aef72476973b88bb6fd023c6784cab2e2506f7
Alexander Burtsev <albburtsev@gmail.com>
2014-08-22 19:43:58 +0400

    Set properties for rules

--- js/iterator.js
+++ js/iterator.js
@@ -14,25 +14,45 @@ function Iterator(tree) {
 }
 
 Iterator.prototype = {
+	STATE_START: 'start',
+	STATE_RULES: 'rules',
+	STATE_DONE: 'done',
+
+	RULE_STATE_START: 'start',
+	RULE_STATE_EXAMPLE: 'example',
+	RULE_STATE_VALID: 'valid',
+	RULE_STATE_INVALID: 'invalid',
+
 	start: function() {
 		var item, rule,
-			state = 'start'; // 'rules', 'done'
+			state = this.STATE_START,
+			ruleState = this.RULE_STATE_START;
 
-		while ( (item = this.next()) && state !== 'done' ) {
+		while ( (item = this.next()) && state !== this.STATE_DONE ) {
 			switch (state) {
-				case 'start':
-					state = (item.isHeader() === 2 && item.text() === 'Rules' && 'rules') || state;
+				case this.STATE_START:
+					state = (item.isHeader() === 2 && item.text() === 'Rules' && this.STATE_RULES) || state;
 					break;
 
-				case 'rules':
-					state = (item.isHeader() === 2 && 'done') || state;
+				case this.STATE_RULES:
+					state = (item.isHeader() === 2 && this.STATE_DONE) || state;
 
 					if ( item.isHeader() === 3 ) {
 						rule = Rule(item.text());
+						ruleState = this.RULE_STATE_START;
 						this.rules.push(rule);
 
+					} else if ( item.isHeader() === 4 && item.text() === 'Example' ) {
+						ruleState = this.RULE_STATE_EXAMPLE;
+
+					} else if ( item.isHeader() === 5 && item.text() === 'Valid' ) {
+						ruleState = this.RULE_STATE_VALID;
+
+					} else if ( item.isHeader() === 5 && item.text() === 'Invalid' ) {
+						ruleState = this.RULE_STATE_INVALID;
+
 					} else if ( item.isParagraph() ) {
-						// @todo
+						this.setRuleProperty(ruleState, item, rule);
 					}
 
 					break;
@@ -41,6 +61,35 @@ Iterator.prototype = {
 					// It's impossible, new state
 					break;
 			}
+		}
+	},
+
+
+
+	setRuleProperty: function(ruleState, item, rule) {
+		var value = item.text(),
+			name;
+
+		switch (ruleState) {
+			case this.RULE_STATE_START:
+				// @todo
+				break;
+
+			case this.RULE_STATE_EXAMPLE:
+				name = this.RULE_STATE_EXAMPLE;
+				break;
+
+			case this.RULE_STATE_VALID:
+				name = this.RULE_STATE_VALID;
+				break;
+
+			case this.RULE_STATE_INVALID:
+				name = this.RULE_STATE_INVALID;
+				break;
+		}
+
+		if ( name ) {
+			rule.set(name, value);
 		}
 	},
 
--- js/rule.js
+++ js/rule.js
@@ -10,7 +10,17 @@ function Rule(name) {
 }
 
 Rule.prototype = {
+	cleanValue: function(name, value) {
+		if ( name === 'example' || name === 'valid' || name === 'invalid' ) {
+			value = value
+				.replace(/^`?js\n/, '')
+				.replace(/\n`$/, '');
+		}
+
+		return value;
+	},
+
 	set: function(name, value) {
-		this[name] = value;
+		this[name] = (this[name] === undefined ? '' : this[name]) + this.cleanValue(name, value);
 	}
 };
